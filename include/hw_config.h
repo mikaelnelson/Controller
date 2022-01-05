def0e239d566bb6ff12ee2d3a93dca0d231eff98
Mikael Nelson <mikaelnelson@gmail.com>
2022-01-04 22:06:43 -0600

    Add hw_config

---
+++ include/hw_config.h
@@ -0,0 +1,50 @@
+#ifndef HW_CONFIG_H
+#define HW_CONFIG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "driver/pcnt.h"
+#include "driver/i2c.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+#define I2C_SCL_PIN                 12
+#define I2C_SDA_PIN                 13
+#define I2C_NUM                     I2C_NUM_0
+#define I2C_FREQ                    400000
+
+#define RPM_PULSE_COUNTER_CHANNEL   PCNT_CHANNEL_0
+#define RPM_PULSE_COUNTER_UNIT      PCNT_UNIT_0
+#define RPM_PULSE_COUNTER_INPUT     15
+#define RPM_PULSE_COUNTER_CONTROL   -1
+
+// PCA9531
+#define SPEEDOMETER_PWM_PIN         0
+#define BACKLIGHT_PWM_PIN           1
+#define ENGINE_KILL_PIN             2
+#define HEADLIGHT_0_PIN             3
+#define HEADLIGHT_1_PIN             4
+#define SWITCH_0_PIN                5
+#define SWITCH_1_PIN                6
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif //HW_CONFIG_H
